deaec597313e7edb922248a6a3f57f6368721abd
Onno Ekker <o.e.ekker@gmail.com>
2012-09-21 20:58:12 +0000

    Default preferences and localized description

---
+++ defaults/preferences/mailredirect.js
@@ -0,0 +1,7 @@
+pref("extensions.{CC3C233D-6668-41bc-AAEB-F3A1D1D594F5}.description", "chrome://mailredirect/locale/mailredirect.properties");
+pref("extensions.mailredirect.copyToSentMail", true);
+pref("extensions.mailredirect.concurrentConnections", 5);
+pref("extensions.mailredirect.debug", false);
+pref("extensions.mailredirect.defaultResentTo", "");
+pref("extensions.mailredirect.defaultResentCc", "");
+pref("extensions.mailredirect.defaultResentBcc", "");
