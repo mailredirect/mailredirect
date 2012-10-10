0a13c04a72952f628567940470cb4908a3438a94
Onno Ekker <o.e.ekker@gmail.com>
2012-10-10 13:30:21 +0000

    Removed unused functions and updated saveConsoleContent

--- chrome/content/mailredirect-prefs.js
+++ chrome/content/mailredirect-prefs.js
@@ -1,77 +1,29 @@
-// need to be global variable
-// List of persisted elements (required by seamonkey to save prefs)
-var _elementIDs = ["copyToSentMail", "debug", "concurrentConnections", "defaultResentTo", "defaultResentCc", "defaultResentBcc"];
+"use strict";
 
-function initPrefs()
-{
-  // initialize the default window values...
-  for (var i = 0; i < _elementIDs.length; i++) {
-    var elementID = _elementIDs[i];
-    var element = document.getElementById(elementID);
-    if (!element) break;
-    var eltType = element.localName;
-    if (eltType == "radiogroup") {
-      var num = element.childNodes[nsPreferences.getIntPref(element.getAttribute("prefstring"),
-          parseInt(element.getAttribute("prefdefval")))];
-      element.selectedItem = document.getElementById(elementID + num);
-    } else if (eltType == "checkbox") {
-      element.checked = nsPreferences.getBoolPref(element.getAttribute("prefstring"),
-          parseInt(element.getAttribute("prefdefval")));
-    } else if (eltType == "textbox") {
-      if (element.getAttribute("preftype") == "int") {
-        element.value = nsPreferences.getIntPref(element.getAttribute("prefstring"),
-            parseInt(element.getAttribute("prefdefval")));
-      } else if (element.getAttribute("preftype") == "string") {
-	element.value = nsPreferences.copyUnicharPref(element.getAttribute("prefstring"));
-      }
-    }
-  }
-}
-
-function savePrefs()
-{
-  for (var i = 0; i < _elementIDs.length; i++) {
-    var elementID = _elementIDs[i];
-
-    var element = document.getElementById(elementID);
-    if (!element) break;
-    var eltType = element.localName;
-
-    if (eltType == "radiogroup") {
-      nsPreferences.setIntPref(element.getAttribute("prefstring"), parseInt(element.value));
-    } else if (eltType == "checkbox") {
-      nsPreferences.setBoolPref(element.getAttribute("prefstring"), element.checked);
-    } else if (eltType == "textbox") {
-      if (element.getAttribute("preftype") == "int") {
-        nsPreferences.setIntPref(element.getAttribute("prefstring"), parseInt(element.value));
-      } else if (element.getAttribute("preftype") == "string") {
-        nsPreferences.setUnicharPref(element.getAttribute("prefstring"), element.value);
-      }
-    }
-  }
-}
+const MODE_WRONLY   = 0x02;
+const MODE_CREATE   = 0x08;
+const MODE_TRUNCATE = 0x20;
 
 function saveConsoleContent()
 {
+  var strbundle = document.getElementById('bundle_mailredirect-prefs');
+
   // open filePicker
-  var filePicker = Components.classes["@mozilla.org/filepicker;1"].createInstance()
-    .QueryInterface(Components.interfaces.nsIFilePicker);
-  filePicker.init(window, "Save JavaScript Console content as", filePicker.modeSave);
-  filePicker.appendFilters(filePicker.filterText);
-  filePicker.appendFilters(filePicker.filterAll);
+  var filePicker = Components.classes["@mozilla.org/filepicker;1"].
+                              createInstance(Components.interfaces.nsIFilePicker);
+  filePicker.init(window, strbundle.getString("saveFile"), Components.interfaces.nsIFilePicker.modeSave);
+  filePicker.appendFilters(Components.interfaces.nsIFilePicker.filterText);
+  filePicker.appendFilters(Components.interfaces.nsIFilePicker.filterAll);
   filePicker.defaultString = "jsconsole.txt";
 
-  if (filePicker.show() == filePicker.returnCancel)
+  if (filePicker.show() == Components.interfaces.nsIFilePicker.returnCancel)
     return;
 
   var file = filePicker.file.QueryInterface(Components.interfaces.nsILocalFile);
-  var fileStream = Components.classes["@mozilla.org/network/file-output-stream;1"]
-    .createInstance(Components.interfaces.nsIFileOutputStream);
+  var fileStream = Components.classes["@mozilla.org/network/file-output-stream;1"].
+                              createInstance(Components.interfaces.nsIFileOutputStream);
 
-  const JS_FILE_NS_WRONLY               = 0x02;
-  const JS_FILE_NS_CREATE_FILE          = 0x08;
-  const JS_FILE_NS_TRUNCATE             = 0x20;
-  fileStream.init(file, JS_FILE_NS_WRONLY | JS_FILE_NS_CREATE_FILE | JS_FILE_NS_TRUNCATE, 0600, null);
+  fileStream.init(file, MODE_WRONLY | MODE_CREATE | MODE_TRUNCATE, parseInt("0600", 8), null);
 
   // for every nsIConsoleMessage save it to file
   var aConsoleService = Components.classes["@mozilla.org/consoleservice;1"]
@@ -85,37 +37,4 @@ function saveConsoleContent()
     fileStream.write(m, m.length);
   }
   fileStream.close();
-}
-
-// functions for seamonkey
-function initSeamonkey()
-{
-  parent.initPanel('chrome://mailredirect/content/mailredirect-prefs-moz.xul');
-}
-
-function doUninstall()
-{
-  var bundleSettings = document.getElementById("bundle_mailredirect-prefs");
-
-  if (window.confirm(bundleSettings.getString("confirmUninstall"))) {
-    var unreg = new exUnregisterer(
-        'chrome://mailredirect/content/contents.rdf',
-        'jar:%chromeFolder%mailredirect.jar!/locale/en-US/mailredirect/contents.rdf',
-        'jar:%chromeFolder%mailredirect-skin-moz.jar!/skin/classic/mailredirect/contents.rdf',
-        'jar:%chromeFolder%mailredirect-skin-moz.jar!/skin/modern/mailredirect/contents.rdf',
-        'jar:%chromeFolder%mailredirect-skin.jar!/skin/classic/mailredirect/contents.rdf'
-        );
-    unreg.unregister();
-
-    if( window.confirm(bundleSettings.getString("confirmDeletePrefs")) ) {
-      unreg.removePrefs('extensions.mailredirect');
-    }
-
-    var button = document.getElementById("uninstallButton");
-    button.setAttribute("label", bundleSettings.getString("uninstalledInfo"));
-    button.setAttribute("disabled", "true");
-
-    window.alert(bundleSettings.getString("infoAfterUninstallation"));
-    // window.close();
-  }
 }
