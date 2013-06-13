acfe5594095fb06b5683088757b220538064000e
Onno Ekker <o.e.ekker@gmail.com>
2013-06-13 13:00:06 +0200

    Rename .xpi to -sm+tb.xpi

--- make-babelzilla.sh
+++ make-babelzilla.sh
@@ -7,7 +7,7 @@ rm -fr babelzilla/
 rsync -a --exclude-from=../code/make-exclude.txt ../code/* babelzilla/
 version=`grep em:version ../code/install.rdf | sed -r "s/^[^>]*>//" | sed -r "s/<.*$//"`
 cd babelzilla
-rm mailredirect-${version}.xpi 2> /dev/null
+rm mailredirect-${version}-sm+tb.xpi 2> /dev/null
 zip -r -D mailredirect-${version}-sm+tb.xpi install.rdf chrome.manifest chrome/ defaults/ LICENSE
 cd ..
 read -p "Press any key to continue . . . " -n 1
