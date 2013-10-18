deacb3f6539b10cd67c852bb95a2851849233de8
Onno Ekker <o.e.ekker@gmail.com>
2013-10-18 15:47:46 +0200

    Added substitution for untranslated strings in dtd files

--- make-amo.sh
+++ make-amo.sh
@@ -29,6 +29,27 @@ do
     fi
   done
 done
+for locale in *
+do
+  for file in ${locale}/*.dtd
+  do
+    echo ${file}
+    grep -q '""' ${file}      # Check if file has untranslated strings
+    if [ $? -eq 0 ]
+    then
+      while read line
+      do
+        if expr match "${line}" '<!ENTITY' > /dev/null
+        then
+          untranslatedstring="${line%%\"*}\"\" -->"
+          untranslatedstring="${untranslatedstring/ENTITY/-- ENTITY}"
+          sed -r "s/${untranslatedstring//\//\\/}/${line//\//\\/}/" ${file} > ${file}.new
+          mv ${file}.new ${file}
+        fi
+      done < en-US/$(basename ${file})
+    fi
+  done
+done
 cd ..
 zip -r -D -0 mailredirect.jar content/ locale/ skin/
 cd ../..
