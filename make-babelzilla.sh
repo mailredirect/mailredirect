9aa9198d9630d16b3d3e211362254fca5d4c14e2
Onno Ekker <o.e.ekker@gmail.com>
2014-04-27 11:21:14 +0200

    Only add en-US locale to BabelZilla version

--- make-babelzilla.sh
+++ make-babelzilla.sh
@@ -4,7 +4,9 @@ cd $(dirname $(cygpath -u $(cygpath -m -s "${0}")))/..
 [ -d xpi ] || mkdir xpi
 cd xpi
 rm -fr babelzilla/
-rsync -a --exclude-from=../code/make-exclude.txt ../code/* babelzilla/
+rsync -a --exclude-from=../code/make-exclude.txt --exclude 'locale/*' ../code/* babelzilla/
+mkdir -p babelzilla/chrome/locale/en-US/
+rsync -a ../code/chrome/locale/en-US/* babelzilla/chrome/locale/en-US/
 version=`grep em:version ../code/install.rdf | sed -r "s/^[^>]*>//" | sed -r "s/<.*$//"`
 cd babelzilla
 rm mailredirect-${version}-sm+tb.xpi 2> /dev/null
