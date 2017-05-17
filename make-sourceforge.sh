eab24039360c076b8326873fed9bd13d5b33836d
oeekker <o.e.ekker@gmail.com>
2017-05-17 21:37:08 +0200

    Update scripts for missing strings and clean-up locales

--- make-sourceforge.sh
+++ make-sourceforge.sh
@@ -13,45 +13,56 @@ rsync -a --exclude-from=../code/make-exclude.txt ../code/* sourceforge/
 cd sourceforge/chrome/locale
 for locale in *
 do
-  for file in ${locale}/*.properties
+  for file in ${locale}/*.*
   do
     echo ${file}
-    grep -q '=$' ${file}      # Check if file has untranslated strings
-    if [ $? -eq 0 ]
+    if [ "${file##*.}" == "properties" ]
     then
-      while read line
-      do
-        if expr index "${line}" '=' > /dev/null
-        then
-          untranslatedstring="^#${line%%=*}=$"
-          untranslatedstring="${untranslatedstring/\#\#/#}"
-          untranslatedstring="${untranslatedstring/\{/\\{}"
-          untranslatedstring="${untranslatedstring/\}/\}}"
-          sed -r "s/${untranslatedstring//\//\\/}/${line//\//\\/}/" ${file} > ${file}.new
-          mv ${file}.new ${file}
-        fi
-      done < en-US/$(basename ${file})
+      # Add missing strings as empty string
+      diff --suppress-common-lines <(cat ${file} | sed -e "s/=.*$/=/") <(cat en-US/$(basename ${file}) | sed -e "s/=.*$/=/") > ${file}.patch
+      patch ${file} ${file}.patch > /dev/null
+      rm ${file}.patch
+
+      # Use original string for empty strings
+      grep -q '=$' ${file}      # Check if file has untranslated strings
+      if [ $? -eq 0 ]
+      then
+        while read line
+        do
+          if expr index "${line}" '=' > /dev/null
+          then
+            untranslatedstring="^${line%%=*}=$"
+            untranslatedstring="${untranslatedstring//\{/\\\{}"
+            untranslatedstring="${untranslatedstring//\}/\\\}}"
+            untranslatedstring="${untranslatedstring//\./\\\.}"
+            sed -r "s/${untranslatedstring//\//\\/}/${line//\//\\/}/" ${file} > ${file}.new
+            mv ${file}.new ${file}
+          fi
+        done < en-US/$(basename ${file})
+      fi
     fi
-  done
-done
-for locale in *
-do
-  for file in ${locale}/*.dtd
-  do
-    echo ${file}
-    grep -q '""' ${file}      # Check if file has untranslated strings
-    if [ $? -eq 0 ]
+    if [ "${file##*.}" == "dtd" ]
     then
-      while read line
-      do
-        if expr match "${line}" '<!ENTITY' > /dev/null
-        then
-          untranslatedstring="${line%%\"*}\"\" -->"
-          untranslatedstring="${untranslatedstring/ENTITY/-- ENTITY}"
-          sed -r "s/${untranslatedstring//\//\\/}/${line//\//\\/}/" ${file} > ${file}.new
-          mv ${file}.new ${file}
-        fi
-      done < en-US/$(basename ${file})
+      # Add missing strings as empty string
+      diff --suppress-common-lines <(cat ${file} | sed -e "s/\".*\"/\"\"/") <(cat en-US/$(basename ${file}) | sed -e "s/\".*\"/\"\"/") > ${file}.patch
+      patch ${file} ${file}.patch > /dev/null
+      rm ${file}.patch
+
+      # Use original string for empty strings
+      grep -q '""' ${file}      # Check if file has untranslated strings
+      if [ $? -eq 0 ]
+      then
+        while read line
+        do
+          if expr match "${line}" '<!ENTITY' > /dev/null
+          then
+            untranslatedstring="${line%%\"*}\"\">"
+            echo ${line}, ${untranslatedstring//\//\\/}
+            sed -r "s/${untranslatedstring//\//\\/}/${line//\//\\/}/" ${file} > ${file}.new
+            mv ${file}.new ${file}
+          fi
+        done < en-US/$(basename ${file})
+      fi
     fi
   done
 done
