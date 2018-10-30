57050e6c2325ef41243b3fcdcfb2d11196b7ca9c
Onno Ekker <o.e.ekker@gmail.com>
2018-10-30 16:06:32 +0100

    Update make-babelzilla.sh to sort files in chrome directory

--- make-babelzilla.sh
+++ make-babelzilla.sh
@@ -12,9 +12,16 @@ rm -fr babelzilla/
 rsync -a --exclude-from=../code/make-exclude.txt --exclude 'locale/*' ../code/* babelzilla/
 mkdir -p babelzilla/chrome/locale/en-US/
 rsync -a ../code/chrome/locale/en-US/* babelzilla/chrome/locale/en-US/
-version=`grep em:version ../code/install.rdf | sed -r "s/^[^>]*>//" | sed -r "s/<.*$//"`
 cd babelzilla
+echo install.rdf > mailredirect.txt
+echo chrome.manifest >> mailredirect.txt
+find chrome -type f | sort >> mailredirect.txt
+echo defaults/ >> mailredirect.txt
+echo icon.png >> mailredirect.txt
+echo LICENSE >> mailredirect.txt
+echo README >> mailredirect.txt
+version=`grep em:version install.rdf | sed -r "s/^[^>]*>//" | sed -r "s/<.*$//"`
 rm mailredirect-${version}-sm+tb.xpi 2> /dev/null
-zip -r -D -9 mailredirect-${version}-sm+tb.xpi install.rdf chrome.manifest chrome/ defaults/ icon.png LICENSE README
+zip -r -D -9 mailredirect-${version}-sm+tb.xpi -@ < mailredirect.txt
 rm mailredirect.txt
 read -p "Press any key to continue . . . " -n 1
